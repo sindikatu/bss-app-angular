bb8bf8e3cefbee80793f8ed219d374398c4a6c1c
sindikatu <gorka@adimedia.net>
2014-12-11 11:00:31 +0100

    solucionar el despegable de los idiomas
    Signed-off-by: sindikatu <gorka@adimedia.net>

--- js/comunes.js
+++ js/comunes.js
@@ -3,9 +3,6 @@ var Bss = Bss || {};
 Bss.lang = Bss.lang || {};
 
 Bss.lang.lang_menu_show_hide = function() {
-	console.log("LLEGA a Bss.lang.lang_menu_show_hide");
-    $('ul.idiomas span').on('click', function() {
-    	console.log("desplegar listado de idiomas");
-        $('ul.idiomas ul').slideToggle();
-    });
+    $('ul.idiomas ul').slideToggle();
 };
+
